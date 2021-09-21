af4f2744360a4fc4594235936831063edc3b369b
caowanjing <227653625@qq.com>
2021-09-21 21:44:45 +0800

    冒泡排序的升级

---
+++ seach-min.js
@@ -0,0 +1,17 @@
+function selectionSort(arr){
+    var len=arr.length;
+    var minIndex,temp;
+    for(var i=0;i<len-1;i++){
+        minIndex=i;
+        for(var j=i+1;j<len;j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex=j;
+            }
+        }
+        temp=arr[i];
+        arr[i]=arr[minIndex];
+        arr[minIndex]=temp;
+
+    }
+    return arr;
+}
